0bcc59d05c2ae3a84d62d4dcfb3d9674e5ffcd9b
dagniel <dagniel@pandora.rand.org>
2022-01-26 06:55:17 -0800

    Update placeholder for estimating mean functions.

--- R/estimate_mu.R
+++ R/estimate_mu.R
@@ -1,8 +1,8 @@
-estimate_mu <- function(data, folds, id, x, y, lrnr) {
+estimate_mu <- function(data, folds, id, x, y, a, lrnr, task_name = 'mu') {
   if (lrnr$predict_type != 'prob') lrnr$predict_type <- 'prob'
   data <- mutate(data, row_id = 1:nrow(data))
 
-  xy_dat <- select(data, any_of(c(x, y)))
+  xy_dat <- select(data, any_of(c(a, x, y)))
   if (!inherits(pull(xy_dat, !!y), 'factor')) {
     xy_dat <- mutate_at(xy_dat, vars(y), as.factor)
   }
@@ -10,12 +10,12 @@ estimate_mu <- function(data, folds, id, x, y, lrnr) {
 
   all_folds <- pull(data, folds)
   unique_folds <- unique(all_folds)
-  incl <- pull(data, include_train)
   predictions <- map_df(unique_folds,
                         ~learn_fold_prob(task = this_task,
-                                         train_ids = which(all_folds != . & incl),
+                                         train_ids = which(all_folds != .),
                                          test_ids = which(all_folds == .),
-                                         lrnr = lrnr))
+                                         lrnr = lrnr,
+                                         a = a))
   # out_ds <- inner_join(data, predictions, by = 'row_id')
 
   select(predictions, !!id, starts_with('mu'))
@@ -24,14 +24,13 @@ estimate_mu <- function(data, folds, id, x, y, lrnr) {
 
 learn_fold_prob <- function(task, train_ids, test_ids, lrnr, a, avals = NULL) {
   lrnr$train(task, row_ids = train_ids)
-  if (is.null(avals)) avals <- task$data %>% pull(!!sym(a)) %>% unique
-  mus <- map(avals, ~predict_unit(lrnr, task, task$data(test_ids), a, .))
-  browser()
-  reduce(mus, full_join)
+  if (is.null(avals)) avals <- task$data() %>% pull(!!sym(a)) %>% unique
+  mus <- map(avals, ~predict_unit(lrnr, task, task$data(test_ids), test_ids, a, .))
+  reduce(mus, inner_join)
 }
 
-predict_unit <- function(lrnr, task, data, a, aval) {
+predict_unit <- function(lrnr, task, data, ids, a, aval) {
   new_ds <- data %>% mutate(!!a := aval)
   predicted_vals <- lrnr$predict_newdata(task, newdata = new_ds)
-  mutate(data, !!glue('mu_{aval}') := predicted_vals[,2])
+  tibble(id = ids, !!glue('mu_{aval}') := predicted_vals$prob[,2])
 }
