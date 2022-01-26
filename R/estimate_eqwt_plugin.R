ba06427ede5322a234098f2610fbc697406b4b68
dagniel <dagniel@pandora.rand.org>
2022-01-26 09:20:37 -0800

    Update plugin estimation.

--- R/estimate_eqwt_plugin.R
+++ R/estimate_eqwt_plugin.R
@@ -1,30 +1,47 @@
 #' Plug-in estimator for unit quality.
 #'
 #' @param data data.frame containing the information to analyze
+#' @param folds optional string identifying the column in `data` that denotes the folds for cross-fitting
+#' @param id string identifying the column in `data` that denotes the ID variable
 #' @param a string identifying the column in `data` that denotes the binary variable indicating the unit of interest
 #' @param y string identifying the column in `data` that denotes the outcome of interest
-#' @param w string identifying the column in `data` that denotes the equity weight
-#' @param e string identifying the column in `data` that denotes the propensity score
-#' @param mu string identifying the column in `data` that denotes the mean function
+#' @param wvars string vector identifying the columns in `data` that denote the protected characteristics
+#' @param zvars string vector identifying the columns in `data` that denote the control (non-protected) characteristics
 #' @param truncation_pt a number in (0,1) to which the propensity score is truncated
 #' @param adaptive logical flag of whether to use the adaptive normalization of Khan and Ugander (2021)
+#' @param K optional integer identifying the number of folds for cross-fitting; required if folds = NULL
+#' @param lrnr mlr3 learner object which will be used to estimate the mean function and propensity
+#' @param epsilon positive scalar that indicates the amount that the optimization is allowed to deviate from the required constraints
 #'
 #' @export
-estimate_eqwt_plugin <- function(data, a, y, w = NULL, e, mu, truncation_pt, adaptive = FALSE) {
-  if (is.null(w)) {
-    dplyr::mutate(data, w = 1)
-    w <- 'w'
-  }
-  if_data <- dplyr::mutate(data,
-            inf_fn = eqwt_influence_fn(a = !!rlang::sym(a),
-                                       y = !!rlang::sym(y),
-                                       e_hat = !!rlang::sym(e),
-                                       w_hat = !!rlang::sym(w),
-                                       mu_hat = !!rlang::sym(mu),
-                                       truncation_pt = truncation_pt,
-                                       adaptive = adaptive
-                                       ))
-  dplyr::summarise(if_data,
-            plugin_est = mean(inf_fn),
-            est_var = var(inf_fn)/dplyr::n())
+estimate_eqwt_plugin <- function(data,
+                                 folds = NULL,
+                                 id,
+                                 a, y, wvars, zvars,
+                                 truncation_pt = 1e-7,
+                                 adaptive = FALSE,
+                                 K = 2,
+                                 lrnr = lrn('classif.ranger'),
+                                 epsilon = 1e-12) {
+  if (is.null(folds)) {
+    ds <- make_folds(data, a, K)
+    folds <- 'fold'
+  } else ds <- data
+  ds <- left_join(ds,
+                  estimate_e(ds, folds, id, c(wvars, zvars), a, lrnr))
+  ds <- left_join(ds,
+                  estimate_mu(ds, folds, id, c(wvars, zvars), y, a, lrnr))
+  ds <- estimate_phi(ds, a, y)
+  avals <- ds %>% pull(!!sym(a)) %>% unique
+  ds <- estimate_equity_wts(ds, avals, wvars, epsilon)
+  phis <- paste0('phi_', avals)
+  ests <- dplyr::summarise_at(ds,
+                      all_of(phis),
+            list(pluginest = ~mean(.*equity_wt),
+            se = ~sqrt(var(.*equity_wt)/dplyr::n())))
+  pivot_longer(ests, cols = everything()) %>%
+    separate(name, into = c('phi', a, 'type'), sep = '_') %>%
+    select(-phi) %>%
+    pivot_wider(id_cols = a, names_from = type, values_from = value)
+
 }
---
+++ R/estimate_phi.R
@@ -0,0 +1,24 @@
+estimate_phi <- function(data, a, y, truncation_pt = 1e-7, adaptive = FALSE) {
+  avals <- unique(pull(data, !!a))
+  mu_nms <- paste0('mu_', avals)
+  e_nms <- paste0('e_', avals)
+  if (!all(mu_nms %in% colnames(data))) stop("mean functions have not been estimated yet. please see `estimate_mu` function.")
+  if (!all(e_nms %in% colnames(data))) stop("propensity scores have not been estimated yet. please see `estimate_e` function.")
+  updated_data <- data
+  for (aa in avals) {
+    this_mu <- paste0('mu_', aa)
+    this_e <- paste0('e_', aa)
+    this_phi <- paste0('phi_', aa)
+    this_a <- paste0('a_', aa)
+    updated_data <- mutate(updated_data,
+                           !!this_a := 1*(!!sym(a) == aa),
+                           !!this_phi := wtd_influence_fn(a = !!sym(this_a),
+                                                          y = y,
+                                                          e_hat = !!sym(this_e),
+                                                          mu_hat = !!sym(this_mu),
+                                                          w_hat = 1,
+                                                          truncation_pt = truncation_pt,
+                                                          adaptive = adaptive))
+  }
+  updated_data
+}
