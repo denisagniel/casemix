d4f09b8447ab0fc24b9168d16450bbc2b5e11b0a
dagniel <dagniel@pandora.rand.org>
2022-03-18 12:37:47 -0700

    Change weight calculation with wrapper function.

---
+++ R/calculate_wts.R
@@ -0,0 +1,49 @@
+#' Calculate weights that obey marginal balance constraints on protected characteristics.
+#'
+#' These weights ensure that each characteristic represented by a variable in `wvars` is balanced in the weighted population.
+#'
+#' @param data data.frame containing the information to analyze
+#' @param a optional string identifying the column in `data` that denotes the units of interest; if not provided, must provide `avals`
+#' @param avals optional string identifying unique values of `a` found in the data; if not provided, must provide `a`
+#' @param wvars string vector identifying the columns in `data` that denote the protected characteristics
+#' @param epsilon positive scalar that indicates the amount that the optimization is allowed to deviate from the required constraints
+#'
+#'
+#' @export
+
+calculate_wts <- function(data, a = NULL, avals = NULL,  wvars, epsilon = 1e-6) {
+  constraints_list <- compute_constraints(data, wvars, epsilon)
+  if (is.null(avals)) {
+    if (is.null(a)) {
+      stop('Must provide either `a` or `avals`.')
+    } else avals <- unique(pull(data, !!sym(a)))
+  }
+  phis <- paste0('phi_', avals)
+  phi_reformat <- reformat_phis(data, phis, wvars, constraints_list$pmf_ds)
+  qp_sln <- try(quadprog::solve.QP(Dmat = diag(phi_reformat$phi_w/sum(phi_reformat$phi_w)),
+                                   Amat = constraints_list$Amat,
+                                   bvec = constraints_list$bvec,
+                                   dvec = rep(0, nrow(phi_reformat))), silent = TRUE)
+
+  if (class(qp_sln) == 'try-error') browser()
+  xi_ds <- select(phi_reformat, all_of(wvars))
+  xi_ds <- mutate(xi_ds, equity_wt = qp_sln$solution)
+  left_join(data, xi_ds, by = wvars)
+}
+
+reformat_phis <- function(data, phis, wvars, pmf_ds, phi_fn = sum) {
+  grp_ds <- group_by_at(data, all_of(wvars))
+  summarise_phi_ds <- summarise_at(grp_ds, all_of(phis), ~sum(.^2))
+  rowwise_phi_ds <- rowwise(ungroup(summarise_phi_ds))
+  out_ds <- mutate(rowwise_phi_ds, phi_w = phi_fn(c_across(phis)))
+
+  ##############################
+  ## make sure the structure of this matrix matches up with the structure used to construct the constraints
+  out_wvars <- select(out_ds, all_of(wvars))
+  pmf_ds_wvars <- select(pmf_ds, all_of(wvars))
+  if (!all(out_wvars == pmf_ds_wvars)) {
+    # browser()
+    stop('the construction of the phis and the constraints do not match.')
+  }
+  ungroup(out_ds)
+}
--- R/estimate_equity_wts.R
+++ R/estimate_equity_wts.R
@@ -10,51 +10,66 @@
 #'
 #'
 #' @export
-estimate_equity_wts <- function(data, a = NULL, avals = NULL,  wvars, epsilon = 1e-6) {
-  constraints_list <- compute_constraints(data, wvars, epsilon)
-  if (is.null(avals)) {
-    if (is.null(a)) {
-      stop('Must provide either `a` or `avals`.')
-    } else avals <- unique(pull(data, !!sym(a)))
-  }
-  phis <- paste0('phi_', avals)
-  phi_reformat <- reformat_phis(data, phis, wvars, constraints_list$pmf_ds)
-  qp_sln <- try(quadprog::solve.QP(Dmat = diag(phi_reformat$phi_w/sum(phi_reformat$phi_w)),
-                                   Amat = constraints_list$Amat,
-                                   bvec = constraints_list$bvec,
-                                   dvec = rep(0, nrow(phi_reformat))), silent = TRUE)
+estimate_equity_wts <- function(data, folds, id, wvars, zvars, a, y, lrnr, separate_e = TRUE, separate_mu = TRUE, epsilon = 1e-12, tune = FALSE, evals = 20, callibrate_e = TRUE, callibrate_mu = TRUE, sub_k = 2, truncation_pt = 1e-7) {
+  all_folds <- pull(data, folds)
+  unique_folds <- unique(all_folds)
 
-  #################################
-  ## trying something
-  g <- t(constraints_list$Amat)
-  h <- constraints_list$bvec
-  a <- diag(ncol(g))
-  b <- rep(1/ncol(g), ncol(g))
-  pmf_ds <- constraints_list$pmf_ds
-
-  lsei_eq <- limSolve::lsei(A = a, B = b, G = g, H = h)
-  lsei_f <- limSolve::lsei(A = a, B = pmf_ds$f, G = g, H = h)
-  ##################################
-
-  if (class(qp_sln) == 'try-error') browser()
-  xi_ds <- select(phi_reformat, all_of(wvars))
-  xi_ds <- mutate(xi_ds, equity_wt = qp_sln$solution, alt_wt1 = lsei_eq$X, alt_wt2 = lsei_f$X)
-  left_join(data, xi_ds)
+  map_df(unique_folds,
+                        ~fold_wt(data,
+                                 folds,
+                                 out_fold = .,
+                                 id,
+                                 wvars,
+                                 zvars,
+                                 a,
+                                 y,
+                                 lrnr = lrnr,
+                                 separate_e,
+                                 separate_mu,
+                                 epsilon,
+                                 tune,
+                                 evals,
+                                 callibrate_e,
+                                 callibrate_mu,
+                                 K = sub_k))
 }
 
-reformat_phis <- function(data, phis, wvars, pmf_ds, phi_fn = sum) {
-  grp_ds <- group_by_at(data, all_of(wvars))
-  summarise_phi_ds <- summarise_at(grp_ds, all_of(phis), ~sum(.^2))
-  rowwise_phi_ds <- rowwise(ungroup(summarise_phi_ds))
-  out_ds <- mutate(rowwise_phi_ds, phi_w = phi_fn(c_across(phis)))
+fold_wt <- function(data,
+                    folds,
+                    out_fold,
+                    id,
+                    wvars,
+                    zvars,
+                    a,
+                    y,
+                    lrnr,
+                    separate_e,
+                    separate_mu,
+                    epsilon,
+                    tune,
+                    evals,
+                    callibrate_e,
+                    callibrate_mu,
+                    K = 2) {
+  train_data <- data %>%
+    filter(!!sym(folds) != out_fold)
+  out_data <- data %>%
+    filter(!!sym(folds) == out_fold)
+  #######################
+  ## create sub-folds on training data
+  train_data <- make_folds(train_data, unit_id = a, K = K, fold_name = 'subfold')
 
-  ##############################
-  ## make sure the structure of this matrix matches up with the structure used to construct the constraints
-  out_wvars <- select(out_ds, all_of(wvars))
-  pmf_ds_wvars <- select(pmf_ds, all_of(wvars))
-  if (!all(out_wvars == pmf_ds_wvars)) {
-    # browser()
-    stop('the construction of the phis and the constraints do not match.')
-  }
-  ungroup(out_ds)
+
+  #######################
+  ## estimate mu and e
+  train_data <- left_join(train_data,
+                  estimate_e(train_data, 'subfold', id, c(wvars, zvars), a, lrnr, separate = separate_e, tune = tune, callibrate = callibrate_e), by = id)
+  train_data <- left_join(train_data,
+                  estimate_mu(train_data, 'subfold', id, c(wvars, zvars), y, a, lrnr, separate = separate_mu, tune = tune, callibrate = callibrate_mu), by = id)
+  train_data <- estimate_phi(train_data, a, y)
+  avals <- unique(dplyr::pull(train_data, !!sym(a)))
+  train_data <- calculate_wts(train_data, avals = avals, wvars = wvars, epsilon = epsilon)
+  eqwt_ds <- train_data %>% select(all_of(wvars), equity_wt) %>% unique
+  out_data %>%
+    inner_join(eqwt_ds, by = wvars)
 }
