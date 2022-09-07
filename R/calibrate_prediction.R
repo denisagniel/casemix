2fbf8bafa95a7c813a890b99d947016a5cb94b3d
dagniel <dagniel@pandora.rand.org>
2022-09-07 08:31:44 -0700

    Add calibration catch for small datasets.

--- R/calibrate_prediction.R
+++ R/calibrate_prediction.R
@@ -10,10 +10,19 @@ calibrate_prediction <- function(pred_ds, join_ds, test_ds, pred_nm, task_y, tar
     group_by(pred_grp) %>%
     summarise(ybar = mean(!!sym(task_y) == target_val),
               predbar = mean(!!sym(pred_nm)))
-  mod_fit <- mgcv::gam(ybar ~ s(predbar, k = 10), data = grp_sum, family = binomial)
+  if (nrow(grp_sum) <= 10) {
+    test_ds <- test_ds %>%
+      inner_join(grp_sum) %>%
+      rename(yhat = ybar)
+  } else {
+    mod_fit <- try(mgcv::gam(ybar ~ s(predbar, k = 10), data = grp_sum, family = binomial), silent = TRUE)
+    if (class(mod_fit) == 'try-error') browser()
 
-  test_ds <- test_ds %>%
-    mutate(yhat = predict(mod_fit, newdata = test_ds, type = 'response'))
+    test_ds <- test_ds %>%
+      mutate(yhat = predict(mod_fit, newdata = test_ds, type = 'response'))
+  }
+
+
   test_ds %>%
     transmute(row_id, !!pred_nm := yhat)
 }
